fd6c6a437d8a1fc0dcf8ed7ceff8ab40e97febf9
Miron Brezuleanu <mbrezu@gmail.com>
2012-01-15 19:47:28 +0200

    Can delete a customer.

--- sorriso.lisp
+++ sorriso.lisp
@@ -71,95 +71,104 @@
              (string-equal (tbnl:script-name tbnl:*request*)
                            str)))
     (cond ((path-is "/")
-           (labels ((output-category (category stream)
-                      (let ((category-name (first category))
-                            (category-content (rest category)))
-                        (with-html-output (stream)
-                          (:h2 :class "section-title" (str category-name)))
-                        (dolist (item category-content)
-                          (with-html-output (stream)
-                            (:p :class "item" (str item)))))))
-             (with-html-output-to-string (hs nil :indent 0)
-               (:html
-                (:head
-                 (:title "Sorriso")
-                 (:link :type "text/css"
-                        :href "/static/css/ui-lightness/jquery-ui-1.8.17.custom.css"
-                        :rel "stylesheet")
-                 (:script :type "text/javascript" :src "/static/js/jquery-1.7.1.min.js")
-                 (:script :type "text/javascript" :src "/static/js/jquery-ui-1.8.17.custom.min.js")
-                 (:script :type "text/javascript" :src "/code/script.js")
-                 (:link :rel "stylesheet" :type "text/css" :media "screen" :href "style.css"))
-                (:body
-                 (:div :id "content"
-                       (:h1 "Sorriso")
-                       (:p :class "phones" "Telefon:"
-                           (:ul (:li "021-316-53-58")
-                                (:li "021-317-65-35")
-                                (:li "0724-376-471")))
-                       (dolist (category (get-sorriso-data))
-                         (output-category category hs)))
-                 (:div :id "order"
-                       (:h2 (str "Comanda curenta"))
-                       (:div :id "add-client" "Adauga client"))
-                 (:div :id "add-client-form"
-                       :title "Adauga client"
-                       (:form
-                        (:fieldset
-                         (:label :for "name" "Nume")
-                         (:input :type "text" :name "name" :id "name")))))))))
+           (generate-root-page))
           ((path-is "/style.css")
-           (css-lite:css
-             (("h2") (:background-color "yellow"))
-             (("#content")
-              (:-moz-column-count "3"
-                                  :-moz-column-gap "20px"
-                                  :-webkit-column-count "3"
-                                  :-webkit-column-gap "20px"
-                                  :column-count "3"
-                                  :column-gap "20px"
-                                  :width "80%"
-                                  ))
-             ((".item") (:font-size "small"))
-             ((".order-item") (:font-size "small"))
-             ((".order-item-button") (:font-size "6px !important"))
-             ((".section-title") (:font-size "small"
-                                             :font-weight "bold"))
-             ((".client-order") (:border-style "solid" :border-width "1px"
-                                               :padding "5px"
-                                               :margin "5px"
-                                               ))
-             (("#order") (:background-color "#eee"
-                                            :width "20%"
-                                            :height "100%"
-                                            :padding-left "10px"
-                                            :position "absolute"
-                                            :top "0px"
-                                            :right "0px"))))
+           (generate-css))
           ((path-is "/code/script.js")
            (generate-script)))))
+
+(defun generate-root-page ()
+  (labels ((output-category (category stream)
+             (let ((category-name (first category))
+                   (category-content (rest category)))
+               (with-html-output (stream)
+                 (:h2 :class "section-title" (str category-name)))
+               (dolist (item category-content)
+                 (with-html-output (stream)
+                   (:p :class "item" (str item)))))))
+    (with-html-output-to-string (hs nil :indent 0)
+      (:html
+       (:head
+        (:title "Sorriso")
+        (:link :type "text/css"
+               :href "/static/css/ui-lightness/jquery-ui-1.8.17.custom.css"
+               :rel "stylesheet")
+        (:script :type "text/javascript" :src "/static/js/jquery-1.7.1.min.js")
+        (:script :type "text/javascript" :src "/static/js/jquery-ui-1.8.17.custom.min.js")
+        (:script :type "text/javascript" :src "/code/script.js")
+        (:link :rel "stylesheet" :type "text/css" :media "screen" :href "style.css"))
+       (:body
+        (:div :id "content"
+              (:h1 "Sorriso")
+              (:p :class "phones" "Telefon:"
+                  (:ul (:li "021-316-53-58")
+                       (:li "021-317-65-35")
+                       (:li "0724-376-471")))
+              (dolist (category (get-sorriso-data))
+                (output-category category hs)))
+        (:div :id "order"
+              (:h2 (str "Comanda curenta"))
+              (:div :id "add-client" "Adauga client"))
+        (:div :id "add-client-form"
+              :title "Adauga client"
+              (:form
+               (:fieldset
+                (:label :for "name" "Nume")
+                (:input :type "text" :name "name" :id "name")))))))))
+
+(defun generate-css ()
+  (css-lite:css
+    (("h2") (:background-color "yellow"))
+    (("#content")
+     (:-moz-column-count "3"
+                         :-moz-column-gap "20px"
+                         :-webkit-column-count "3"
+                         :-webkit-column-gap "20px"
+                         :column-count "3"
+                         :column-gap "20px"
+                         :width "80%"
+                         ))
+    ((".item") (:font-size "small"))
+    ((".order-item") (:font-size "small"))
+    ((".order-item-button") (:font-size "6px !important"))
+    ((".section-title") (:font-size "small"
+                                    :font-weight "bold"))
+    ((".client-order") (:padding "5px"
+                                 :margin "5px"
+                                 :background-color "#ffb040"
+                                 :border-radius "6px"
+                                 :-webkit-border-radius "6px"
+                                 :-moz-border-radius "6px"))
+    (("#order") (:background-color "#eee"
+                                   :width "20%"
+                                   :height "100%"
+                                   :padding-left "10px"
+                                   :position "absolute"
+                                   :top "0px"
+                                   :right "0px"))))
 
 (defun generate-script ()
   (let ((*ps-print-pretty* t))
     (ps
-      (defun make-order-item (ui)
-        (let ((element ($ "<div/>"))
-              (close-button ($ "<span/>"))
-              (text ($ "<span/>")))
-          ((@ element add-class) "order-item")
+      (defun make-close-button (element)
+        (let ((close-button ($ "<span/>")))
           ((@ close-button text) "X")
           ((@ close-button add-class) "order-item-button")
           (chain ((@ close-button button))
                  (click (lambda ()
-                          ((@ element remove)))))
+                          ((@ element remove)))))))
+      (defun make-order-item (ui)
+        (let ((element ($ "<div/>"))
+              (text ($ "<span/>")))
+          ((@ element add-class) "order-item")
           ((@ text text) ((@ ui helper text)))
-          ((@ element append) close-button text)
+          ((@ element append) (make-close-button element) text)
           element))
       (defun make-client-area (client-name)
         (let ((element ($ "<div/>"))
-              (title ($ "<p/>")))
+              (title ($ "<span/>")))
           ((@ title text) client-name)
-          ((@ element append) title)
+          ((@ element append) (make-close-button element) title)
           ((@ element add-class) "client-order")
           ((@ element droppable)
            (create "drop" (lambda (event ui)
